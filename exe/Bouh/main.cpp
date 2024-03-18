6f802b46499727ff7168633d8971146580a960ff
Keisuke Miyako <keisuke.miyako@4d.com>
2024-03-18 12:16:11 +0900

    windows exe

---
+++ exe/Bouh/main.cpp
@@ -0,0 +1,20 @@
+﻿//
+//  main.c
+//  bouh
+//
+//  Created by miyako on 2024/03/18.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+
+    std::string in1, in2;
+    std::cin >> in1;
+    std::cout << in1;
+    std::cin >> in2;
+    std::cout << in2;
+
+    return 0;
+}
